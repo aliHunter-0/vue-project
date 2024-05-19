8a827e4756c25c6315d13d99cce7dfdef8edc448
Ali Talib <joestartl89@gmail.com>
2024-05-19 18:14:47 +0300

    fix ui issue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
 import SideBar from './components/SideBar.vue';
 </script>
 
 <template>
   <SideBar />
-  <div class="container mx-auto px-32">
-    <RouterView />
-  </div>
 </template>
 
 <style scoped></style>
